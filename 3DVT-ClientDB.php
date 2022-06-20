18a12b5c77e788d488ff309dbb0bb9d3de634d46
np31-Nik <71725581+np31-Nik@users.noreply.github.com>
2022-06-20 17:21:34 +0200

    Update 3DVT-ClientDB.php

--- 3DVT-ClientDB.php
+++ 3DVT-ClientDB.php
@@ -8,3 +8,53 @@ Description: Custom client database for agency Costa Soñada.
 Version: 1.0.0
 Author: Costa Soñada
 */
+
+
+//If someone tries to access this besides Wordpress, it exits.
+if (!defined('ABSPATH')){
+ die('Can\t access this.');
+}
+
+//On plugin activation
+register_activation_hook(__FILE__, "activate_myplugin");
+
+//On plugin deactivation
+register_deactivation_hook(__FILE__,"deactivate_myplugin");
+
+function activate_myplugin(){
+ 
+}
+
+function deactivate_myplugin(){
+ 
+}
+
+function init_db_myplugin(){
+ /*
+     global $table_prefix, $wpdb;
+
+    $tblname = 'pin';
+    $wp_track_table = $table_prefix . "$tblname ";
+
+    #Check to see if the table exists already, if not, then create it
+
+    if($wpdb->get_var( "show tables like '$wp_track_table'" ) != $wp_track_table) 
+    {
+
+        $sql = "CREATE TABLE `". $wp_track_table . "` ( ";
+        $sql .= "  `id`  int(11)   NOT NULL auto_increment, ";
+        $sql .= "  `pincode`  int(128)   NOT NULL, ";
+        $sql .= "  PRIMARY KEY `order_id` (`id`) "; 
+        $sql .= ") ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ; ";
+        require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
+        dbDelta($sql);
+    }
+    */
+}
+
+add_action('admin_menu','test_plugin_setup_menu');
+
+function test_plugin_setup_menu(){
+    add_menu_page( 'Test Plugin Page', 'Test Plugin', 'manage_options', 'test-plugin', 'test_init' );
+}
+
