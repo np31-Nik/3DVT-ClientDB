eceb99e514b0903512313dcda85db109a2f6615c
Nik <np31@alu.ua.es>
2022-07-23 15:04:04 +0200

    searchbar3

--- search.php
+++ search.php
@@ -3,7 +3,20 @@
 
 <div id="searchBar">
 <h1>search bar</h1>
-
+<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+    <label>
+        <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
+        <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+    </label>
+    <label for="color">Background Color:</label>
+<select name="color" id="color">
+	<option value="">--- Choose a color ---</option>
+	<option value="red">Red</option>
+	<option value="green">Green</option>
+	<option value="blue">Blue</option>
+</select>
+    <input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+</form>
 
 
 
