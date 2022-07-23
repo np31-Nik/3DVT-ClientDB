ad5223a158b394fb134d24732ef669c3613d9aa3
Nik <np31@alu.ua.es>
2022-07-23 16:48:11 +0200

    SearchBar

--- search.php
+++ search.php
@@ -1,11 +1,49 @@
 <?php 
+include_once("wp-config.php");
+include_once("wp-includes/wp-db.php");
+
 $message = "";
+$sql ="";
+$results="";
+
 if(isset($_POST['SubmitButton'])){ //check if form was submitted
   $name = $_POST['name']; //get input text
   $tipo = $_POST['operationType'];
   $captado =$_POST['captado'];
   $message = "...Searching for: | Nombre: ".$name." | Tipo: ".$tipo." | Captado: ".$captado." |";
-}   
+
+  searchClients($name,$tipo,$captado);
+}
+
+function searchClients($name,$tipo,$captado){
+    $conditions="";
+    $table="";
+
+    if($name!="" or $tipo!="" or $captado!=""){
+        $conditions=$conditions." WHERE ";
+    }
+
+    if ($name !=""){
+        $conditions=$conditions."name='".$name."'";
+    }
+    if($tipo !=""){
+        if($name!=""){
+            $conditions=$conditions." AND";
+        }
+        $conditions=$conditions." tipoOperacion='".$tipo."'";
+    }
+    if($captado !=""){
+        if($tipo!=""){
+            $conditions=$conditions." AND";
+        }
+        $conditions=$conditions." captadoPor='".$captado."'";
+    }
+
+
+    $sql = "SELECT * FROM ".$table.$conditions;
+
+    $results = $wpdb->get_results($sql);
+}
 ?>
 
 <header>
@@ -31,8 +69,10 @@ if(isset($_POST['SubmitButton'])){ //check if form was submitted
 
     <br>
     <?php echo $message; ?>
+    <br>
+    <?php echo "SQL query: ".$sql?>
+    <br>
+    <?php echo "Results: ".$results?>
     </form>
-
-
 </div>
 </header>
