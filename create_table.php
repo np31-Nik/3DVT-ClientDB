10a715f3a1c6ba4a1cd1853cd8c3fc3513ef68e1
Hiep Do <101442297+HiepDo24@users.noreply.github.com>
2022-07-27 10:28:56 -0700

    Add files via upload

---
+++ create_table.php
@@ -0,0 +1,23 @@
+<?php
+
+global $wpdb;
+
+function create_table() {
+$table_name = $wpdb->prefix."3dvt_clients";
+$charset_collate=$wpdb->get_charset_collate();
+
+$sql = "CREATE TABLE IF NOT EXISTS ".$table_name." (
+    PRIMARY KEY id INTEGER(10) NOT NULL AUTO_INCREMENT,
+    name varchar(40) NOT NULL,
+    phone varchar(30) NOT NULL,
+    email varchar(40) NOT NULL,
+    operation varchar(55) NOT NULL,
+    found varchar(20) NOT NULL,
+    date date NOT NULL,
+    comment varchar(255) NOT NULL
+    ) $charset_collate;";
+
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+
+}
